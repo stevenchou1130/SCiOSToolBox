7675940022cc2f374447e3bb5805076a4d280c96
Steven C <stevennote1130@gmail.com>
2021-12-17 14:26:29 +0800

    First commit

---
+++ SCiOSToolbox/Source/TestingClass.swift
@@ -0,0 +1,17 @@
+//
+//  TestingClass.swift
+//  SCiOSToolbox
+//
+//  Created by Steven C on 2021/12/17.
+//
+
+import Foundation
+
+public class TestingClass {
+    
+    public let text: String
+    
+    public init(text: String) {
+        self.text = text
+    }
+}
